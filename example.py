afb1ac1f685396214d1d6f01d63c4eb155ef0cda
0xnu <oketunjifinbarrs@hotmail.com>
2021-06-20 13:52:57 +0100

    bug fixes
    Signed-off-by: 0xnu <oketunjifinbarrs@hotmail.com>

--- example.py
+++ example.py
@@ -4,13 +4,13 @@ from coronavirus.covid19api import Coronavirusapi
 covid = Coronavirusapi("public_user", "public_pass")
 
 
-# Get May 2021 Data
-response = covid.getTimeSeries("may2021")
+# Get June 2021 Data
+response = covid.getTimeSeries("jun2021")
 print(response)
 
 
 # Get Time Series Data
-response = covid.getTimeSeries("time_series_recovered_global")
-print(response)
+# response = covid.getTimeSeries("time_series_recovered_global")
+# print(response)
 
 
